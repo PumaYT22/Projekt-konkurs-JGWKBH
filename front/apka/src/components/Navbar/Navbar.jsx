9fc6edc3eea37988c43f0d384d05b0c694cbbefa
PumaYT <pumaxyt@gmail.com>
2025-03-14 10:52:18 +0100

    s

--- front/apka/src/components/Navbar/Navbar.jsx
+++ front/apka/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
-        <h2 className='text-xl font-medium text-black py-2'>Nazwa</h2>
+        <h2 className='text-xl font-medium text-black py-2'>Aurion</h2>
 
     </div>
   )
